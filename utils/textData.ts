1f27295ad9080c8975fc877ecd9496e112161d98
Dawit Alemu <dndawita@gmail.com>
2024-07-06 21:48:31 -0400

    started textData util

---
+++ utils/textData.ts
@@ -0,0 +1,213 @@
+export const textData = {
+    header: {
+        home: {
+            english: "Home",
+            amharic: ""
+        },
+        about: {
+            english: "About",
+            amharic: "ስለእኔ በጥቂቱ"
+        },
+        contact: {
+            english: "Contact",
+            amharic: "መልእክትዎን ይላኩልኝ"
+        },
+        listings: {
+            english: "Listings",
+            amharic: "ለሽያጭ የቀረቡ ቤቶ ች"
+        },
+        wishlist: {
+            title: {
+                english: "Wishlist",
+                amharic: ""
+            },
+            emptyMessage: {
+                english: "There are no items in your wishlist",
+                amharic: ""
+            }
+        },
+    },
+    home: {
+        hero: {
+            title: {
+                english: "Elias Realtor",
+                amharic: "ቤት ሲገዙም ሲሸጡም ሆነ ሲያከራዩ መብቶንና የሚገባውዎትን ሕጋዊ ጥቅሞን ለማስከበር  በሙያዊ ብቃትና በትጋት ሳንታክት በቅንነት እንሠራለን።"
+            },
+            subtitle: {
+                english: "We Always Work In Your Best Interest When You Buy, Rent, & Sell",
+                amharic: ""
+            },
+        },
+        resources: {
+            assistance: {
+                homeBuyers: {
+                    title: {
+                        english: "",
+                        amharic: ""
+                    },
+                    md: {
+                        english: "",
+                        amharic: ""
+                    },
+                    dc: {
+                        english: "",
+                        amharic: ""
+                    },
+                    va: {
+                        english: "",
+                        amharic: ""
+                    }
+                },
+                hud: {
+                    title: {
+                        english: "",
+                        amharic: ""
+                    },
+                    hud: {
+                        english: "",
+                        amharic: ""
+                    },
+                    md: {
+                        english: "",
+                        amharic: ""
+                    },
+                    dc: {
+                        english: "",
+                        amharic: ""
+                    },
+                    va: {
+                        english: "",
+                        amharic: ""
+                    }
+                }
+            },
+            connections: {
+                lenders: {
+                    title: {
+                        english: "Mortgage Lenders & Insurances",
+                        amharic: "ለቤት ግዢ የብድር አገልግሎትና ኢንሹራንስ"
+                    },
+                    contact: [
+                        {
+                            company: "",
+                            name: "",
+                            title: "",
+                            nmls: "",
+                            number: "",
+                            address: "",
+                            website: ""
+                        },
+                        {
+                            company: "",
+                            name: "",
+                            title: "",
+                            nmls: "",
+                            number: "",
+                            address: "",
+                            website: ""
+                        },
+                        {
+                            company: "",
+                            name: "",
+                            title: "",
+                            nmls: "",
+                            number: "",
+                            address: "",
+                            website: ""
+                        }
+                    ]
+                },
+                contractors: {
+                    title: {
+                        english: "General Contractors",
+                        amharic: ""
+                    },
+                    contact: [
+                        {
+                            name: "",
+                            services: "",
+                            perks: "",
+                            number: "",
+                            email: "",
+                            website: ""
+                        }
+                    ]
+                }
+            },
+        },
+    },
+    about: {
+        title: {
+            english: "About",
+            amharic: "ስለእኔ በጥቂቱ"
+        },
+        biography: {
+            title: {
+                english: "Biography",
+                amharic: ""
+            },
+            paragraph: {
+                english: `
+                    Elias Ekoubazgie is a devoted family man, having been happily married for over twenty years and raising three beautiful children. Elias boasts an impressive educational background, having earned a bachelor’s degree in electrical engineering technology, as well as two associate degrees in biomedical engineering and automotive service technology. He also holds certificates in Networking, Project Management, and is a licensed realtor in Maryland and Virginia .
+                    <br><br>
+                    With a dual career in Project Management and Real Estate, Elias draws from his personal experiences in buying and selling residential properties in Indiana and Maryland to provide his clients with exceptional service. He firmly believes in empowering his clients by providing them with accurate, up-to-date, and timely information, allowing them to make informed decisions.
+                    <br><br>
+                    Prior to entering the Real Estate industry, Elias gained over two decades of experience in customer service, a field he is passionate about. His dedication to his clients has been recognized by his current manager, who praised him as an "amazing customer advocate."
+                    <br><br>
+                    In addition to his professional pursuits, Elias is also an active member of the Greater Capital Area Association of Realtors (GCAAR) and the National Association of Realtors (NAR), further demonstrating his commitment to the industry and his clients.
+                `,
+                amharic: ""
+            }
+        },
+        mission: {
+            title: {
+                english: "Mission Statement",
+                amharic: ""
+            },
+            paragraph: {
+                english: "My unwavering mission is to safeguard and advance your valuable investment in your home by equipping you with the most precise, top-notch, cutting-edge, and up-to-date information that empowers you to make informed, sound decisions. My pledge is to prioritize your best interest and work tirelessly to ensure that you achieve your real estate goals with complete satisfaction.",
+                amharic: ""
+            }
+        }
+    },
+    contact: {
+        title: {
+            english: "Contact",
+            amharic: "ኤልያስ ሪልተርን ለማግነኘት"
+        },
+        form: {
+            title: {
+                english: "Send Message",
+                amharic: "መልእክትዎን እዚህ ይላኩልኝ"
+            },
+            name: {
+                english: "Name",
+                amharic: "ስም"
+            },
+            email: {
+                english: "Email",
+                amharic: ""
+            },
+            body: {
+                english: "Tell me about your dream home!",
+                amharic: "ስሞን ስልክና የኢሜይል አድራሻዎን ካሰፈሩ በቀጥታ መገናኘት እንችላለን።"
+            },
+            submit: {
+                english: "Send",
+                amharic: ""
+            }
+        },
+        info: {
+            title: {
+                english: "Contact Info",
+                amharic: "ኤልያስ ሪልተር"
+            }
+        }
+    },
+    listings: {
+        title: {
+            english: "Listings",
+            amharic: "መከራየት ይፈልጋሉ"
+        }
+    }
+};
